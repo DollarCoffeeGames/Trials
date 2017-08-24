d1fa0b399778a19850c81bc9ec3dbf09ea09c9e3
Bonez-laptop\Bonez <a.armstrong197@gmail.com>
2017-08-24 18:46:31 -0400

    UI update

--- Assets/Script/UI/UIManager.cs
+++ Assets/Script/UI/UIManager.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour 
 {
     int utilityPoints;
+    
 
     GameManager gm;
+    public GameObject trapPanel;
+    public GameObject menuButton;
+    public GameObject pauseMenu;
+    int resourcePoints;
 
     // Use this for initialization
     void Start () {
-
+        trapPanel.SetActive(false);
+        pauseMenu.SetActive(false);
+        menuButton.SetActive(true);
         gm = GetComponent<GameManager>();
+        resourcePoints = 100;
 		
 	}
 	
@@ -31,6 +40,45 @@ public class UIManager : MonoBehaviour
     {
         //mouse over stat display for traps
         //opens panels contained in buttons
+    }
+
+    public void ToggleMenu()
+    {
+        if (trapPanel.activeSelf == true)
+        {
+            trapPanel.SetActive(false);
+        }
+        else
+        {
+            trapPanel.SetActive(true);
+        }
+    }
+
+    void ToggleMenuButton()
+    {
+        if(menuButton.activeSelf == true)
+        {
+            menuButton.SetActive(false);
+        }
+        else
+        {
+            menuButton.SetActive(true);
+        }
+    }
+
+    private void TogglePause()
+    {
+        if (Time.timeScale>0)
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+
+        }
+        else if (Time.timeScale==0)
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        }
     }
 
 }
